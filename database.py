7fe2ad7d4710729f2a6949e53821d46fa10aea0d
1is7ac3 <isaac.qa13@gmail.com>
2024-08-09 00:27:41 -0400

    change table product and option drop table

--- database.py
+++ database.py
@@ -1,17 +1,15 @@
-'''Import modules'''
+"""Import modules"""
 import csv
 import mysql.connector as maria
 import data
 
 
 def create_database_mariadb():
-    '''funcion crear database inventory'''
-    print(data.db_config_init)
+    """funcion crear database inventory"""
     connection = maria.connect(**data.db_config_init)
     try:
         cursor = connection.cursor()
-        cursor.execute('''CREATE DATABASE IF NOT EXISTS inventory''')
-        print('Se creo la base de datos inventory')
+        cursor.execute("""CREATE DATABASE IF NOT EXISTS inventory""")
     except maria.Error as err:
         print(err)
     cursor.close()
@@ -19,11 +17,11 @@ def create_database_mariadb():
 
 
 def run_query_mariadb(query, parameters=()):
-    '''funcion correr busqueda'''
+    """funcion correr busqueda"""
     conn = None
     cursor = None
     try:
-        conn = maria.connect(**data.db_config)
+        conn = maria.Connect(**data.db_config)
         cursor = conn.cursor()
         cursor.execute(query, parameters)
         result = cursor.fetchall()
@@ -40,11 +38,11 @@ def run_query_mariadb(query, parameters=()):
 
 
 def run_query_mariadb_edit(query, parameters=()):
-    '''funcion correr edit'''
+    """funcion correr edit"""
     conn = None
     cursor = None
     try:
-        conn = maria.connect(**data.db_config)
+        conn = maria.Connect(**data.db_config)
         cursor = conn.cursor()
         cursor.execute(query, parameters)
         conn.commit()
@@ -62,17 +60,18 @@ def run_query_mariadb_edit(query, parameters=()):
 
 
 def create_table_mariadb():
-    '''funcion crear tablas'''
-    connection = maria.connect(**data.db_config)
+    """funcion crear tablas"""
+    connection = maria.Connect(**data.db_config)
     try:
         cursor = connection.cursor()
-        cursor.execute(''' CREATE TABLE IF NOT EXISTS product (
-                        id INT AUTO_INCREMENT PRIMARY KEY, code VARCHAR(255),
-                        name VARCHAR(255) UNIQUE, quantity INT, price_buy INT,
-                        ganancia INT, price_sales INT, location VARCHAR(255)
-                        ) ''')
+        cursor.execute(
+            """ CREATE TABLE IF NOT EXISTS product (
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        code VARCHAR(255) UNIQUE, name VARCHAR(255),
+                        price_buy INT, ganancia INT, price_sales INT,
+                        location INT, location2 INT, location3 INT) """)
         connection.commit()
-        print('Se creo la tabla product')
+
     except maria.errors.IntegrityError as err:
         print(err)
     cursor.close()
@@ -80,15 +79,17 @@ def create_table_mariadb():
 
 
 def create_table_sales_mariadb():
-    '''funcion crear tablas'''
-    connection = maria.connect(**data.db_config)
+    """funcion crear tablas"""
+    connection = maria.Connect(**data.db_config)
     try:
         cursor = connection.cursor()
-        cursor.execute(''' CREATE TABLE IF NOT EXISTS sales (
+        cursor.execute(
+            """ CREATE TABLE IF NOT EXISTS sales (
                         id INT AUTO_INCREMENT PRIMARY KEY,
-                        name VARCHAR(255) UNIQUE, quantity INT, price INT) ''')
+                        name VARCHAR(255) UNIQUE, quantity INT, price INT) """
+        )
         connection.commit()
-        print('Se creo la tabla de sales')
+
     except maria.errors.IntegrityError as err:
         print(err)
     cursor.close()
@@ -96,15 +97,17 @@ def create_table_sales_mariadb():
 
 
 def create_database_users():
-    '''funcion crear table users'''
-    connection = maria.connect(**data.db_config)
+    """funcion crear table users"""
+    connection = maria.Connect(**data.db_config)
     try:
         cursor = connection.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS users(
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS users(
            id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) UNIQUE,
-           password VARCHAR(255))''')
+           password VARCHAR(255))"""
+        )
         connection.commit()
-        print('Se creo la tabla users')
+
     except maria.errors.IntegrityError as err:
         print(err)
     cursor.close()
@@ -112,15 +115,17 @@ def create_database_users():
 
 
 def create_database_history():
-    '''funcion crear table users'''
-    connection = maria.connect(**data.db_config)
+    """funcion crear table history"""
+    connection = maria.Connect(**data.db_config)
     try:
         cursor = connection.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS history(
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS history(
             id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), price INT,
-            sale VARCHAR(255), date VARCHAR(255))''')
+            quantity INT, sale VARCHAR(255), date VARCHAR(255))"""
+        )
         connection.commit()
-        print('Se creo la tabla users')
+
     except maria.errors.IntegrityError as err:
         print(err)
     cursor.close()
@@ -128,42 +133,52 @@ def create_database_history():
 
 
 def csv_import(file_name):
-    '''Funcion import cvs'''
-    with open(file_name, 'r', encoding='utf-8') as csv_file:
-        reader = csv.reader(csv_file, delimiter=';')
+    """Función import cvs"""
+    with open(file_name, "r", encoding="utf-8") as csv_file:
+        reader = csv.reader(csv_file, delimiter=";")
         for row in reader:
-            query = '''INSERT INTO product (code, name, quantity,
-            price_buy, ganancia, price_sales, location)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)'''
-            price = round(float(row[3]) * (1 + (float(row[4]) / 100)))
+            query = """INSERT INTO product (code, name, price_buy, ganancia,
+            price_sales, location, location2, location3)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+            price = round(float(row[2]) * (1 + (float(row[3]) / 100)))
             parameters = (str(row[0].upper()), str(row[1].upper()),
                           round(float(row[2])), round(float(row[3])),
-                          round(float(row[4])), price, str(row[5].upper()))
+                          price, round(float(row[4])), round(float(row[5])),
+                          round(float(row[6])))
             result = run_query_mariadb_edit(query, parameters)
             print(result, maria.errorcode.ER_DUP_ENTRY)
             if result == 2:
-                query_db_exist = '''select quantity, price_buy, ganancia,
-                    location, name from product'''
+                query_db_exist = """select price_buy, ganancia,
+                    location, location2, location3, code from product"""
                 db_rows = run_query_mariadb(query_db_exist)
                 for i in db_rows:
-                    if i[4] == row[1]:
-                        cantidad = i[0]
-                query = '''UPDATE product SET quantity=%s, price_buy=%s,
-                ganancia=%s, price_sales=%s, location=%s WHERE name=%s'''
-                price_2 = round(float(row[3]) * (1 + (float(row[4]) / 100)))
-                new_c = int(row[2]) + int(cantidad)
-                parameters = (new_c, row[3], row[4], price_2,
-                              str(row[5].upper()), row[1])
-                run_query_mariadb_edit(query, parameters)
+                    if i[5] == row[0]:
+                        query = """UPDATE product SET price_buy=%s,
+                        ganancia=%s, price_sales=%s, location=%s,
+                        location2=%s, location3=%s WHERE name=%s"""
+                        price_2 = round(float(row[2]) * (1 + (float(
+                            row[3]) / 100)))
+                        parameters = (row[2], row[3], price_2,
+                                      round(int(row[4])+int([2])),
+                                      round(int(row[5])+int([3])),
+                                      round(int(row[6])+int([4])))
+                        run_query_mariadb_edit(query, parameters)
     csv_file.close()
 
+
 def csv_export(file_name):
-    '''Funcion export cvs'''
-    with open(file_name, 'w', encoding='utf-8') as csv_file:
-        write = csv.writer(csv_file, delimiter=';')
-        query = '''select id, code, name, quantity, price_buy, ganancia,
-        price_sales, location from product order by name desc'''
-        db_rows =run_query_mariadb(query)
+    """Función export cvs"""
+    with open(file_name, "w", encoding="utf-8") as csv_file:
+        write = csv.writer(csv_file, delimiter=";")
+        query = """select code, name, price_buy, ganancia, location, location2,
+        location3 from product order by name desc"""
+        db_rows = run_query_mariadb(query)
         for x in db_rows:
             write.writerow(x)
     csv_file.close()
+
+
+def change():
+    "Change datebase"
+    query = """DROP table product"""
+    run_query_mariadb_edit(query)
